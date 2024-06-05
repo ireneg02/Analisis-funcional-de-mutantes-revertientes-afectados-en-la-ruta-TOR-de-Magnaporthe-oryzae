7bd978fde81b0c5a73ec14de1d40f29974e434b7
ireneg02 <167988096+ireneg02@users.noreply.github.com>
2024-06-05 10:56:15 +0200

    Update ensamblado_SOAPdenovo2.sh

--- ensamblado_SOAPdenovo2.sh
+++ ensamblado_SOAPdenovo2.sh
@@ -4,6 +4,7 @@
 #SBATCH --partition=long
 #SBATCH --time=2-00:00:00          # total run time limit in HH:MM:SS
 
+#Cargamos los módulos y el programa:
 module use /beegfs/easybuild/CentOS/7.6.1810/Skylake/modules/all
 module use /beegfs/easybuild/common/modules/all
 module load SOAPdenovo2/r242
