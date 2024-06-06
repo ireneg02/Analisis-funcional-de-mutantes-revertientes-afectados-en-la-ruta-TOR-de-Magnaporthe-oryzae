845b0d97990f27125be782aade55bd04ab170492
ireneg02 <167988096+ireneg02@users.noreply.github.com>
2024-06-06 15:50:49 +0200

    Create ensamblado_MaSuRCA_Chromosome_Scaffolder.sh

---
+++ ensamblado_MaSuRCA_Chromosome_Scaffolder.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+#SBATCH --mem=200GB
+#SBATCH --job-name MaSuRCA_Chromosaome_Scaffolder
+#SBATCH --partition=long
+#SBATCH --time=1-59:00:20          # total run time limit in HH:MM:SS
+
+#Cargamos los módulos 
+module use /beegfs/easybuild/CentOS/7.6.1810/Skylake/modules/all
+module use /beegfs/easybuild/common/modules/all
+#Activamos nuestro usuario de mamba para poder acceder a MaSuRCA
+mamba init
+mamba activate irenegmasurca
+
+cd /home/ireneg/DATOS_DNA-SEQ/procesamiento/ #Nos desplazamos a la carpeta con las carpetas de las muestras
+for folder in $(ls -d C*/); do #Recorremos las carpetas de las muestras, en las que se encuentran las lecturas de la secuenciación
+        cd $folder #Entramos en la carpeta
+        file="${folder:0:18}" #Obtenemos el nombre de la muestra 
+        chromosome_scaffolder.sh -r "/home/ireneg/DATOS_DNA-SEQ/originales/MagorGY11_1/Mycocosm/Assembly/Genome_Assembly_(unmasked)//MagorGY11_1_AssemblyScaffolds.fasta" -q CA/primary.genome.scf.fasta -t 30 -nb #Ejecutamos chromosome scaffolder
+        cd .. #Salimos de la carpeta de la muestra
+done
+
+#-r permite indicar a continuación el genoma de referencia al que se van a referenciar los scaffolds
+#-q permite indicar el ensamblaje cuyos scaffolds se van a referenciar al genoma de referencia
+#-t indica el número de threads a utilizar
