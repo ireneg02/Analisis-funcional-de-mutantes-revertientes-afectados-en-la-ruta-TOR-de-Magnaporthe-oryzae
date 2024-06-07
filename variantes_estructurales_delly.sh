992f463b1c0fa355ab6a947e817ac63f46b1fbb7
ireneg02 <167988096+ireneg02@users.noreply.github.com>
2024-06-07 13:25:49 +0200

    Update variantes_estructurales_delly.sh

--- variantes_estructurales_delly.sh
+++ variantes_estructurales_delly.sh
@@ -1,19 +1,21 @@
-# Define variables
+#Este análisis se realizó en Galaxy, pero la transcripción de las opciones usadas es:
+
+# Definir variables
 BAM_FILE="/path/to/input_0.bam"
 BAM_INDEX_FILE="/path/to/input_0.bam.bai"
 GENOME_FILE="/path/to/genome.fa"
 OUTFILE="result.bcf"
 VCF_FILE="result.vcf"
 
-# Create symbolic links
+# Crear links simbólicos
 ln -s "${BAM_FILE}" 'input_0.bam'
 ln -s "${BAM_INDEX_FILE}" 'input_0.bam.bai'
 ln -s "${GENOME_FILE}" 'genome.fa'
 
-# Run delly call
+# Correr Delly
 delly call --svtype ALL --genome genome.fa --outfile "${OUTFILE}" --map-qual 1 --qual-tra 20 --mad-cutoff 9 --minclip 25 --min-clique-size 2 --minrefsep 25 --maxreadsep 40 --geno-qual 5 'input_0.bam'
 
-# Check if result.bcf exists and convert to VCF format
+# Comprobar si result.bcf existe y convertir a formato VCF
 if test -f "${OUTFILE}"; then
   bcftools view "${OUTFILE}" > "${VCF_FILE}"
 else
